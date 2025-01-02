7711d0e169f761228d73d2e2df74f11ffc40464f
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-01-02 14:30:43 +0530

    final schema changes

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
-    email: { type: String, unique: true} 
+    email: { type: String, unique: true} ,
     username: String,
     password: String
 });
